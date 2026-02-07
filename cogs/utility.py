458b61c602a68d0ea606e72b1881292df2e59e8f
Mohit <mohitmano06@gmail.com>
2026-02-07 20:51:52 +0530

    Update utility.py
    Added runtime feature to check the latency of the bot with the server

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,4 +1,6 @@
 import random
+import datetime
+import time
 from discord.ext import commands
 
 class Utility (commands.Cog):
@@ -9,9 +11,13 @@ class Utility (commands.Cog):
     async def roll(self,ctx):
         await ctx.send(f"You rolled {random.randint(1,6)} 🎲")
 
-    @commands.command(help = "Tells your ping")
+    @commands.command(help = "Tells your ping and runtime")
     async def ping(self,ctx):
-
+        start_time = time.perf_counter()
+        message = await ctx.send("Testing Ping...")
+        end_time = time.perf_counter()
+        
+        runtime = (end_time - start_time) * 1000
         latency = round(self.bot.latency *1000)
 
         if latency > 250:
@@ -23,7 +29,13 @@ class Utility (commands.Cog):
         else: 
             msg = "Ping is good stop whining "
 
-        await ctx.send(f"{latency}ms - {msg}")
+        await message.edit(content=f"**Server Runtime:** {runtime:.2f}ms\n**Bot Ping:** {latency}ms\n{msg}")
+
+    @commands.command(help = "Shows bot uptime")
+    async def uptime(self, ctx):
+        uptime = datetime.datetime.utcnow() - self.bot.start_time
+        uptime = str(uptime).split('.')[0]
+        await ctx.send(f"Current Uptime: {uptime}")
 
 async def setup(bot):
     await bot.add_cog(Utility(bot))
